c8988e82d296757fba02e73d3182ca367066aa57
Ben King <bking2015@gmail.com>
2017-01-01 18:36:56 -0500

    asfdsd

--- VideoMirror/test2/webcam.js
+++ VideoMirror/test2/webcam.js
@@ -1,23 +1,39 @@
-if(navigator.webkitGetUserMedia!=null) {
-  var options = {
-    video:true,
-    audio:true
-  };
-  navigator.webkitGetUserMedia(options,
-    function(stream) {
-      var video = document.querySelector('video');
-      video.src = window.webkitURL.createObjectURL(stream);
-    },
-    function(e) {
-      console.log("There was a problem");
-    }
-  );
-}
 
-document.querySelector('video').addEventListener('ended',myHandler,false);
-function myHandler(e) {
-    console.log('ended');
-    setTimeout(function(){
-        document.querySelector('video').play();
-    }, 5000);
+var constraints = {
+  video:true,
+  audio:true
+};
+
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then(function(mediaStream){
+    var video = document.querySelector('video');
+    video.src = window.webkitURL.createObjectURL(mediaStream);
+    video.onloadedmetadata = function(e){
+        video.play();
+    };
+  })
+  .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+
+
 }
+//
+// if(navigator.webkitGetUserMedia!=null) {
+//
+//   navigator.webkitGetUserMedia(options,
+//     function(stream) {
+//
+//     },
+//     function(e) {
+//       console.log("There was a problem");
+//     }
+//   );
+// }
+//
+// document.querySelector('video').addEventListener('ended',myHandler,false);
+// function myHandler(e) {
+//     console.log('ended');
+//     setTimeout(function(){
+//         document.querySelector('video').play();
+//     }, 5000);
+// }
